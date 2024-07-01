31fbc8cbd64af6950003e019ad347fb058475219
Bhuvanesh Selvaraj <bhuvanesh.sel@gmail.com>
2024-06-30 23:06:15 -0700

    Add function to check for win

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-const gameBoard = () => {
+const gameBoard = (function() {
     const rows = 3;
     const columns = 3;
-    const board = [];
-    for (i = 0; i < rows; i++) {
-        for (j =0; i < columns; j++) {
-            board[i][j] = cell();
-        }
-    }
+    let board = [["","",""],["","",""],["","",""]];
+    
 
     const getBoard = () => board;
 
     const checkCellAvailability = (row, column) => {
-        if (getBoard()[row][column] === "-") {
+        if (getBoard()[+row][+column] === "") {
             return true;
         }
         else {
@@ -19,25 +15,24 @@ const gameBoard = () => {
         }
     }
 
-    const cell = () => {
-        let value = "-";
-
-        const markBoard = (player) => {
-            value = player;
-        }
-
-        const getValue = () => value;
-        return {
-            getValue, markBoard
-        };
-    }
+    
 
     return {
-        getBoard, checkCellAvailability, cell
+        getBoard, checkCellAvailability
     }
-}
 
-function players(playerOneName = "Player One", playerTwoName = "Player Two") {
+})();
+
+
+const players = function(playerOneName = "Player One", playerTwoName = "Player Two")  {
+    let playerOneTurn = true;
+
+    const getPlayerTurn = () => playerOneTurn;
+
+    const setPlayerTurn = (value) => {
+        playerOneTurn = value;
+    }
+
     const playerOne = {
         playerOneName,
         value: "X"
@@ -49,24 +44,86 @@ function players(playerOneName = "Player One", playerTwoName = "Player Two") {
     }
 
     return {
-        playerOne, playerTwo
+        playerOne, playerTwo, getPlayerTurn, setPlayerTurn
     };
-}
+}();
 
-const displayController = () => {
+const displayController = function()  {
+
+    let playGame = false;
     const container = document.querySelector(".container");
-    const 
+    const playButton = document.querySelector(".play");
+    const resetButton = document.querySelector(".reset"); 
+
+    playButton.addEventListener("click", (e) => {
+        playGame = true;
+    })
 
     container.addEventListener("click", (e) => {
         const row = e.target.classList[0].split("-")[1];
         const column = e.target.classList[1].split("-")[1];
-        if (gameBoard.checkCellAvailability(row, column)) {
+        console.log(gameBoard.checkCellAvailability(row,column));
+        console.log(players.getPlayerTurn());
+        console.log(playGame);
+        if (gameBoard.checkCellAvailability(row, column) && playGame) {
+            if (players.getPlayerTurn() === true) {
+                gameBoard.getBoard()[row][column] = "X";
+                console.log(e.target);
+                e.target.textContent = "X"
+            }
+            else {
+                gameBoard.getBoard()[row][column] = "O";
+                e.target.textContent = "O"; 
+            }
+            players.setPlayerTurn(!players.getPlayerTurn());
+            console.log(gameFlow.checkForWin());
+
+            
         }
     })
-}
 
-displayController();
 
-const gameFlow = () => {
+}();
 
-}
+
+const gameFlow = function() {
+
+    let board = gameBoard.getBoard();
+
+    const checkForWin = () => {
+        if (board[0][0] === "X" && board[0][1] === "X" && board[0][2] === "X" ||
+            board[1][0] === "X" && board[1][1] === "X" && board[1][2] === "X" ||
+            board[2][0] === "X" && board[2][1] === "X" && board[2][2] === "X" ||
+            board[0][0] === "X" && board[1][0] === "X" && board[2][0] === "X" ||
+            board[0][1] === "X" && board[1][1] === "X" && board[2][1] === "X" ||
+            board[0][2] === "X" && board[1][2] === "X" && board[2][2] === "X" ||
+            board[0][0] === "X" && board[1][1] === "X" && board[2][2] === "X" ||
+            board[0][2] === "X" && board[1][1] === "X" && board[2][0] === "X"
+        )
+            {
+                alert("Player One Wins!");
+                reset();
+
+            }
+            if (board[0][0] === "O" && board[0][1] === "O" && board[0][2] === "O" ||
+                board[1][0] === "O" && board[1][1] === "O" && board[1][2] === "O" ||
+                board[2][0] === "O" && board[2][1] === "O" && board[2][2] === "O" ||
+                board[0][0] === "O" && board[1][0] === "O" && board[2][0] === "O" ||
+                board[0][1] === "O" && board[1][1] === "O" && board[2][1] === "O" ||
+                board[0][2] === "O" && board[1][2] === "O" && board[2][2] === "O" ||
+                board[0][0] === "O" && board[1][1] === "O" && board[2][2] === "O" ||
+                board[0][2] === "O" && board[1][1] === "O" && board[2][0] === "O"
+            )
+                {
+                    alert("Player Two Wins!");
+                    reset();
+                }
+    }
+    
+    const reset = () => {
+        board = [["","",""],["","",""],["","",""]];
+
+    }
+    return {checkForWin};
+}();
+
